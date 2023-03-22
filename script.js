f02a701cc86dd191ac4ff665a2871fdcc073a722
João Victor Ribeiro <joao.leiterib19@gmail.com>
2023-03-21 21:20:50 -0300

    Finalização da lógica do jogo

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let choice = '';
+let housesPoints = 0;
+let yourPoints = 0;
 
 document.querySelector('.rules').addEventListener('click', ()=>{
     document.querySelector('.rulesArea').style.display = 'flex';
@@ -21,6 +23,8 @@ document.querySelector('.scissors').addEventListener('click', ()=>{
     game(choice);
 });
 
+document.querySelector('.restart').addEventListener('click', restart);
+
 function game(c){
     let icon = document.querySelector('.yourIcon');
 
@@ -38,54 +42,131 @@ function game(c){
 
 function checkGame(c){
     let housesChoice = '';
+    let icon = document.querySelector('.housesIcon');
 
     house().then(function(resultado){
         housesChoice = resultado; 
+
+        icon.innerHTML = `<img src="images/icon-${resultado}.svg"/>`;
+        icon.classList.add(`c-${resultado}`);
+        icon.style.width = '180px';
+        icon.style.height = '180px';
+        icon.style.backgroundColor = '#FFF';
+
+        setTimeout(() => {checkWinner(housesChoice, c)} , 1000);
     });
-    
+}
+
+function checkWinner(housesChoice, c){
+    let result = '';
+
     switch(c){
         case 'paper':
             if(housesChoice === 'paper'){
+                result = 'empate';
                 console.log('empate');
             }
             if(housesChoice === 'rock'){
+                result = 'ganhou';
                 console.log('Você ganhou');
             }
             if(housesChoice === 'scissors'){
+                result = 'perdeu';
                 console.log('Você perdeu');               
             }
             
         break;
         case 'rock': 
             if(housesChoice === 'paper'){
+                result = 'perdeu';
                 console.log('Você perdeu');
             }
             if(housesChoice === 'rock'){
+                result = 'empate';
                 console.log('empate');
             }
             if(housesChoice === 'scissors'){
+                result = 'ganhou';
                 console.log('Você ganhou');               
             }
         break;
         case 'scissors':
             if(housesChoice === 'paper'){
+                result = 'ganhou';
                 console.log('Você ganhou');
             }
             if(housesChoice === 'rock'){
+                result = 'perdeu';
                 console.log('Você perdeu');
             }
             if(housesChoice === 'scissors'){
+                result = 'empate';
                 console.log('empate');               
             }
         break;
     }
+
+    showResult(result);
 }
 
 function house(){
     return new Promise(function(resolve, reject){
-        console.log("calculando");
+        
+        document.querySelector('.housesIcon').innerHTML = "Carregando..."
         setTimeout(function(){
-            resolve('paper');
-        }, 3000);
+            resolve(choosing());
+        }, 1000);
     })
+}
+
+function choosing(choosenIcon){
+    let housePlay = Math.floor(Math.random() * 3);
+    
+    if(housePlay == 0){choosenIcon = 'paper'};
+    if(housePlay == 1){choosenIcon = 'rock'};
+    if(housePlay == 2){choosenIcon = 'scissors'};
+
+    return choosenIcon;
+}
+
+function showResult(r){
+    let icon = document.querySelector('.showResult');
+
+    document.querySelector('.result').style.display = 'block';
+
+    if( r === 'empate'){icon.innerHTML = 'EMPATOU!';}
+    if( r === 'ganhou'){
+        icon.innerHTML = 'VOCÊ GANHOU!';
+        yourPoints++;
+    }
+    if( r === 'perdeu'){
+        icon.innerHTML = 'VOCÊ PERDEU!';
+        housesPoints++;
+    }
+
+    document.querySelector('.player .realScore').innerHTML = yourPoints;
+    document.querySelector('.house .realScore').innerHTML = housesPoints;
+}
+
+function restart(){
+    document.querySelector('.one').style.display = 'flex';
+    document.querySelector('.two').style.display = 'none';
+    document.querySelector('.result').style.display = 'none';
+    
+    let youricon = document.querySelector('.yourIcon');
+    let housesicon = document.querySelector('.housesIcon');
+
+    housesicon.classList.remove('c-paper');
+    housesicon.classList.remove('c-rock');
+    housesicon.classList.remove('c-scissors');
+    youricon.classList.remove('c-paper');
+    youricon.classList.remove('c-rock');
+    youricon.classList.remove('c-scissors');
+
+    housesicon.style.width = '120px';
+    housesicon.style.height = '120px';
+    housesicon.style.backgroundColor = 'hsl(229, 25%, 31%)';
+    youricon.style.width = '120px';
+    youricon.style.height = '120px';
+    youricon.style.backgroundColor = 'hsl(229, 25%, 31%)';
 }
